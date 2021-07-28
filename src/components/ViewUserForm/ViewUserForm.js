b032dfbb99391658661a786de40e8090c0766263
Nathanael Grier <Nathanael.Grier@Smoothstack.com>
2021-07-28 14:37:51 -0500

    imports fixed

--- src/components/ViewUserForm/ViewUserForm.js
+++ src/components/ViewUserForm/ViewUserForm.js
@@ -1,9 +1,9 @@
 import axios from "axios"
-import User from "./components/layout/User"
+import User from "../layout/User"
 import { useState } from "react"
-import AuthContext from "./store/auth-context"
+import AuthContext from "../../store/auth-context"
 import { useContext } from "react"
-import UpdateUserForm from './components/UpdateUserForm/UpdateUserForm'
+import UpdateUserForm from "../UpdateUserForm/UpdateUserForm"
 import { Button } from "react-bootstrap"
 
 function ViewUserForm() {
