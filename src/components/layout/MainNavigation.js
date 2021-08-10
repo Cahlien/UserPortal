a17639d34be84fd7d670b6bb8c09ac5176481b5f
Nathanael Grier <nathanael.grier@smoothstack.com>
2021-08-10 17:35:08 -0500

    logout repaired

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -31,7 +31,7 @@ function MainNavigation(props) {
                     </li>
                     <li className="nav-item">
                        {!authContext.userIsLoggedIn && <Link className={'nav-link'} to={'/auth'}>Log In</Link>}
-                       {authContext.userIsLoggedIn && <Link className={'nav-link'} to={'/'}>Logout</Link>}
+                       {authContext.userIsLoggedIn && <Link className={'nav-link'} onClick={logout} to={'/'}>Logout</Link>}
                     </li>
                     <li className="nav-item">
                        {authContext.userIsLoggedIn && <Link className={'nav-link'} to={'/me'}>My Details</Link>}
