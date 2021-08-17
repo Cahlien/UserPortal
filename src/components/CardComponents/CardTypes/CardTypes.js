80924245497f998c0b57cc971aec2f74953b0a38
Matthew Crowell <>
2021-08-17 09:46:34 -0500

    add pagination to view cards on offer

--- src/components/CardComponents/CardTypes/CardTypes.js
+++ src/components/CardComponents/CardTypes/CardTypes.js
@@ -1,4 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
+import Pagination from '@material-ui/lab/Pagination';
 import axios from "axios";
 import {useHistory} from "react-router-dom";
 import ActionContext from "../../../store/action-context";
@@ -22,28 +23,38 @@ function CardTypes(props) {
     const token = authContext.token;
     const [availableCards, setAvailableCards] = useState();
     const [cardsDisplayed, setCardsDisplayed] = useState(false);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [pageSize, setPageSize] = useState(10);
+    const [searchCriteria, setSearchCriteria] = useState();
+    const [sortBy, setSortBy] = useState("id,asc");
     const url = 'http://localhost:9001/cards/available'
+    const [numberOfPages, setNumberOfPages] = useState(10);
+    const pageSizes = [2, 5, 10, 15, 20, 25]
+
+    /**
+     * This function retrieves the list of cards the bank offers and updates the page's state.
+     *
+     * @returns {Promise<void>}
+     */
+    async function getList() {
+        const params = {search: searchCriteria, page: currentPage, size: pageSize, sortBy:sortBy};
+
+        const list = await axios.get(url, {
+            params: params,
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (list.data.content !== availableCards) {
+            setAvailableCards(list.data.content);
+            setNumberOfPages(list.data.content.totalPages);
+            setCardsDisplayed(true);
+        }
+    }
 
     useEffect(() => {
-        /**
-         * This function retrieves the list of cards the bank offers and updates the page's state.
-         *
-         * @returns {Promise<void>}
-         */
-        async function getList() {
-            const list = await axios.get(url, {
-                headers: {
-                    'Authorization': token,
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            if (list.data.content !== availableCards) {
-                setAvailableCards(list.data.content);
-                setCardsDisplayed(true);
-            }
-        }
-
         if (!cardsDisplayed) {
             getList();
         }
@@ -61,6 +72,42 @@ function CardTypes(props) {
     }
 
     /**
+     * This function is called whenever the page number is changed and adjusts
+     * the state of the component to send the appropriate request and render
+     * the page correctly given the updated page number.
+     *
+     * @param event the event fired by changing the page number
+     * @param value the value selected for the new current page
+     */
+    function handlePageChange(event, value){
+        setCardsDisplayed(false);
+        setCurrentPage(value-1);
+    }
+
+    /**
+     * This function is called whenever the search criteria is changed and adjusts
+     * the state of the component to send the appropriate request and render the
+     * component correctly given the updated search criteria.
+     *
+     * @param event the event fired by changing the search criteria
+     * @param value the value entered for the new search criteria
+     */
+    function handleSearchCriteriaChange(event){
+        setSearchCriteria(event.target.value);
+    }
+
+    /**
+     * This function is called whenever the page size is changed.
+     *
+     * @param event the event fired by selecting a new page size
+     */
+    function handlePageSizeChange(event){
+        setPageSize(event.target.value);
+        setCurrentPage(0);
+        setCardsDisplayed(false);
+    }
+
+    /**
      * This function sets the targetId in the action context and moves the user to
      * the sign up page for the selected card.
      *
@@ -73,7 +120,29 @@ function CardTypes(props) {
 
     return (
         <section className={'container'}>
+            <h1 className={'text-center mt-5'}>The Cards of BeardTrust</h1>
             <div className={'mt-5'}>
+                <div>
+                    <div className={'input-group mb-3'}>
+                        <div className={'me-5 col-xs-12 col-lg-2'}>
+                        <span className={'align-middle'}>
+                            {'Items per Page: '}
+                        </span>
+                            <select className={'text-center align-middle'} onChange={handlePageSizeChange} value={pageSize}>
+                                {pageSizes.map((size) => (
+                                    <option key={size} value={size}>{size}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <span className={'text-center col-sm-0 col-md-4 col-lg-6 col-xl-7'}></span>
+
+                        <input type={'text'} className={'form-control col-xs-12 col-md-2'} placeholder={'Search'} value={searchCriteria}
+                               onChange={handleSearchCriteriaChange}/>
+                            <button className={'btn btn-outline-secondary'} type={'button'} onClick={getList}>Search</button>
+
+                    </div>
+                </div>
+
                 <Table striped bordered hover style={{marginRight: 5 + 'px'}}>
                     <thead>
                     <tr>
@@ -102,6 +171,7 @@ function CardTypes(props) {
                     ))}
                     </tbody>
                 </Table>
+                <Pagination className={'my-3'} count={numberOfPages} page={currentPage} siblingCount={1} boundaryCount={1} onChange={handlePageChange} />
             </div>
         </section>
     );
