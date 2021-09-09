20d22f272a8039212076b2fe4887d7d878d224f8
Matthew Crowell <>
2021-09-09 06:35:30 -0500

    restore pagination, sorting, and filtering to user cards page

--- src/components/CardComponents/UserCards/UserCards.js
+++ src/components/CardComponents/UserCards/UserCards.js
@@ -2,6 +2,7 @@ import {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import AuthContext from "../../../store/auth-context";
 import CardsList from "./CardsList";
+import Pagination from '@material-ui/lab/Pagination';
 
 /**
  * This function returns a page that retrieves and displays a list of all
@@ -19,6 +20,7 @@ function UserCards(){
     const url = 'http://localhost:9001/cards/' + userId + "/all";
     const [cardsList, setCardsList] = useState();
     const [cardsDisplayed, setCardsDisplayed] = useState(false);
+    const pageSizes = [5, 10, 15, 20, 25, 50, 100];
 
     useEffect(() => {
         /**
@@ -48,10 +50,31 @@ function UserCards(){
 
     return(
         <section className={'container'}>
-            <ul>
-                <h2 className={'d-flex my-3'}>Your Cards</h2>
-                <CardsList cards={cardsList}/>
-            </ul>
+            <div className={'mt-5'}>
+                <h2 className={'my-3 text-center'}>Your Cards</h2>
+                <div>
+                    <div className={'input-group mb-3'}>
+                        <div className={'me-5 col-xs-12 col-lg-2'}>
+                            <span className={'align-middle'}>
+                                {'Items per Page: '}
+                            </span>
+                            <select data-testid={'pageSizeSelector'} className={'text-center align-middle'}>
+                                {pageSizes.map((size) => (
+                                    <option key={size} value={size}>{size}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <span className={'text-center col-sm-0 col-md-4 col-lg-6'}/>
+                        <input type={'text'} className={'form-control'} placeholder={'Search'} />
+                        <button className={'btn btn-outline-secondary'} type={'button'}
+                                id={'searchBar'}>Search
+                        </button>
+                    </div>
+                    <CardsList className={'my-3'} cards={cardsList}/>
+                    <Pagination className={'my-3'} siblingCount={1}
+                                boundaryCount={1} />
+                </div>
+            </div>
         </section>
     );
 }
