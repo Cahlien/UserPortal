8ca1f7c79a93849255612c17a8380e004db7c80d
Matthew Crowell <>
2021-10-20 14:33:17 -0500

    add sorting and filtering

--- src/components/TransactionComponents/TransactionsList.js
+++ src/components/TransactionComponents/TransactionsList.js
@@ -8,38 +8,155 @@ import axios from "axios";
 function TransactionsList(props) {
     const authContext = useContext(AuthContext);
     const url = props.url;
-    const transactions = props.transactions;
     const pageSizes = [5, 10, 15, 20, 25, 50];
+    const [modified, setModified] = useState();
+    const [transactions, setTransactions] = useState();
     const [pageSize, setPageSize] = useState(5);
-    const [sortBy, setSortBy] = useState();
+    const [sortBy, setSortBy] = useState('statusTime,asc');
     const [searchCriteria, setSearchCriteria] = useState();
     const [numberOfPages, setNumberOfPages] = useState(10);
     const [currentPage, setCurrentPage] = useState(1);
+    const [sortByDate, setSortByDate] = useState({active: false, name: 'statusTime', direction: 'asc'});
+    const [sortByDescription, setSortByDescription] = useState({active: false, name: 'notes', direction: 'asc'});
+    const [sortByAmount, setSortByAmount] = useState({active: false, name: 'transactionAmount', direction: 'asc'});
+    const [sortByStatus, setSortByStatus] = useState({active: false, name: 'transactionStatus', direction: 'asc'});
 
-    useEffect(async () => {
+    const getTransactions = async () => {
         console.log("Effect called.");
+        setModified(false);
 
-        let txt = await axios.get(url, {
-            headers: { "Authorization": localStorage.getItem("token") }
+        let content = await axios.get(url, {
+            params: {
+                page: currentPage === 0 ? 0 : currentPage - 1,
+                size: pageSize,
+                sort: sortBy,
+                search: searchCriteria
+            },
+            headers: {
+                "Authorization": authContext.token
+            }
         });
 
-        console.log(txt)
-    })
-
-    function handlePageSizeChange() {
-
+        setTransactions(content.data.content);
+        setNumberOfPages(content.data.totalPages);
+        console.log(content);
     }
 
-    function getList() {
+    useEffect(() => {
+        getList();
+        console.log("SortBy after getList(): " + sortBy);
+    });
 
+    function handlePageSizeChange(event) {
+        setPageSize(event.target.value);
+        setCurrentPage(1);
+        setModified(true);
     }
 
-    function handleSearchCriteriaChange() {
+    async function getList() {
+        if(modified !== false){
+            await getTransactions();
+        }
 
+        setModified(false);
     }
 
-    function handlePageChange() {
+    function handleSearchCriteriaChange(event) {
+        const criteria = event.target.value;
+        if(criteria.length > 0){
+            setSearchCriteria(criteria);
+        } else {
+            setSearchCriteria();
+        }
+    }
 
+    function handlePageChange(event, value) {
+        setCurrentPage(value);
+        setModified(true);
+    }
+
+    function deactivateSortParams(){
+        setSortByDate({active: false, name: 'statusTime', direction: sortByDate.direction});
+        setSortByDescription({active: false, name: 'notes', direction: sortByDescription.direction});
+        setSortByAmount({active: false, name: 'transactionAmount', direction: sortByAmount.direction});
+        setSortByStatus({active: false, name: 'transactionStatus', direction: sortByStatus.direction});
+    }
+
+    function onSortChanged(event) {
+        let sort = '';
+
+        deactivateSortParams();
+
+        if(event.target.id === 'statusTime'){
+            if(sortByDate.active === true){
+                if(sortByDate.direction === 'asc'){
+                    setSortByDate({active: true, name: 'statusTime', direction: 'desc'});
+                } else {
+                    setSortByDate({active: true, name: 'statusTime', direction: 'asc'});
+                }
+            } else {
+                setSortByDate({active: true, name: 'statusTime', direction: 'desc'});
+            }
+
+            sort = sortByDate.name + ',' + sortByDate.direction;
+            console.log("sortBy should include '&sort=" + sortByDate.name + "," + sortByDate.direction + "'");
+        }
+
+        if(event.target.id === 'notes'){
+            if(sortByDescription.active === true){
+                if(sortByDescription.direction === 'asc'){
+                    setSortByDescription({active: true, name: 'notes', direction: 'desc'});
+                } else {
+                    setSortByDescription({active: true, name: 'notes', direction: 'asc'});
+                }
+            } else {
+                setSortByDescription({active: true, name: 'notes', direction: 'desc'});
+            }
+
+            sort = sortByDescription.name + ',' + sortByDescription.direction;
+            console.log("sortBy should include '&sort=" + sortByDate.name + "," + sortByDate.direction + "'");
+        }
+
+        if(event.target.id === 'transactionAmount'){
+            if(sortByAmount.active === true){
+                if(sortByAmount.direction === 'asc'){
+                    setSortByAmount({active: true, name: 'transactionAmount', direction: 'desc'});
+                } else {
+                    setSortByAmount({active: true, name: 'transactionAmount', direction: 'asc'});
+                }
+            } else {
+                setSortByAmount({active: true, name: 'transactionAmount', direction: 'desc'});
+            }
+
+            sort = sortByAmount.name + '.dollars,' + sortByAmount.direction;
+            console.log("sortBy should include '&sort=" + sortByDate.name + "," + sortByDate.direction + "'");
+        }
+
+        if(event.target.id === 'transactionStatus'){
+            if(sortByStatus.active === true){
+                if(sortByStatus.direction === 'asc'){
+                    setSortByStatus({active: true, name: 'transactionStatus', direction: 'desc'});
+                } else {
+                    setSortByStatus({active: true, name: 'transactionStatus', direction: 'asc'});
+                }
+            } else {
+                setSortByStatus({active: true, name: 'transactionStatus', direction: 'desc'});
+                sort = sortByStatus.name + ',' + sortByStatus.direction;
+            }
+
+            sort = sortByStatus.name + ',' + sortByStatus.direction;
+            console.log("sortBy should include '&sort=" + sortByDate.name + "," + sortByDate.direction + "'");
+        }
+
+        console.log("sort: " + sort);
+
+        setModified(true);
+        setSortBy(sort);
+        console.log('SortBy: ' + sortBy);
+    }
+
+    function handleSearchClicked() {
+        setModified(true);
     }
 
     return (
@@ -64,7 +181,7 @@ function TransactionsList(props) {
                             <input id={'searchBar'} type={'text'} className={'form-control'} placeholder={'Search'}
                                    value={searchCriteria}
                                    onChange={handleSearchCriteriaChange}/>
-                            <button className={'btn btn-outline-secondary'} type={'button'} onClick={getList}
+                            <button className={'btn btn-outline-secondary'} type={'button'} onClick={handleSearchClicked}
                                     id={'searchBar'}>Search
                             </button>
                         </div>
@@ -81,24 +198,24 @@ function TransactionsList(props) {
                             <th className={'align-middle text-center'} data-sortable={'true'}
                                 scope={'col'}
                                 id={'statusTime'}
-                                onClick={props.onSortChanged}>Date
-                                {props.sortOrder === 'statusTime,asc' && ' ↑'}
-                                {props.sortOrder === 'statusTime,desc' && ' ↓'}
+                                onClick={onSortChanged}>Date
+                                {(sortByDate.active === true && sortByDate.direction === 'desc') && ' ↑'}
+                                {(sortByDate.active === true && sortByDate.direction === 'asc') && ' ↓'}
                             </th>
                             <th data-sortable={'true'} scope={'col'} id={'notes'} colSpan={'1'}
-                                onClick={props.onSortChanged}>Description
-                                {props.sortOrder === 'notes,asc' && ' ↑'}
-                                {props.sortOrder === 'notes,desc' && ' ↓'}
+                                onClick={onSortChanged}>Description
+                                {(sortByDescription.active === true && sortByDescription.direction === 'desc') && ' ↑'}
+                                {(sortByDescription.active === true && sortByDescription.direction === 'asc') && ' ↓'}
                             </th>
                             <th className={'align-middle text-center'} data-sortable={'true'} scope={'col'}
-                                id={'transactionAmount.dollars'} onClick={props.onSortChanged}>Amount
-                                {props.sortOrder === 'transactionAmount.dollars,asc' && ' ↑'}
-                                {props.sortOrder === 'transactionAmount.dollars,desc' && ' ↓'}
+                                id={'transactionAmount'} onClick={onSortChanged}>Amount
+                                {(sortByAmount.active === true && sortByAmount.direction === 'desc') && ' ↑'}
+                                {(sortByAmount.active === true && sortByAmount.direction === 'asc') && ' ↓'}
                             </th>
                             <th className={'align-middle text-center'}
-                                id={'transactionStatus'} onClick={props.onSortChanged}>Status
-                                {props.sortOrder === 'transactionStatus,asc' && ' ↑'}
-                                {props.sortOrder === 'transactionStatus,desc' && ' ↓'}
+                                id={'transactionStatus'} onClick={onSortChanged}>Status
+                                {(sortByStatus.active === true && sortByStatus.direction === 'desc') && ' ↑'}
+                                {(sortByStatus.active === true && sortByStatus.direction === 'asc') && ' ↓'}
                             </th>
                         </tr>
                         </thead>
